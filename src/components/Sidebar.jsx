05e0901ca70746df8ec01dbd9e7c7a03c54a0177
ShreoshreeSingha <102968693+ShreoshreeSingha@users.noreply.github.com>
2023-03-29 14:54:43 +0530

    update

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,172 @@
+import React, { useContext, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import SidebarMenu from "./SidebarMenu";
+import {
+  adminRoutes,
+  manufacturerRoutes,
+  distributorRoutes,
+  retailerRoutes,
+  storageRoutes,
+} from "../data/link";
+import { AuthContext } from "../contexts/auth-context";
+import { useMediaQuery } from "@material-ui/core";
+
+const Sidebar = () => {
+  const { userRole } = useContext(AuthContext);
+  var routes = "";
+  if (userRole === "admin@gmail.com") {
+    routes = adminRoutes;
+  } else if (userRole === "manufacturer@gmail.com") {
+    routes = manufacturerRoutes;
+  } else if (userRole === "distributor@gmail.com") {
+    routes = distributorRoutes;
+  } else if (userRole === "storage@gmail.com") {
+    routes = storageRoutes;
+  } else {
+    routes = retailerRoutes;
+  }
+
+  const activeLink =
+    "grid items-center gap-1 p-2 rounded-lg text-black font-semibold text-md m-2 bg-active-bg";
+  const normalLink =
+    "grid items-center gap-1 p-2 rounded-lg text-md font-semibold text-white dark:text-gray-200 hover:text-[#7b8cb8] m-2";
+
+  const isSmallScreen = useMediaQuery("(max-width: 640px)");
+  
+
+  return (
+    <>
+      {/* {!isSmallScreen && ( */}
+        <div className="h-screen pb-10 pt-2 z-50">
+          <div className="flex justify-center items-center">
+            <Link>
+              <img
+                src={require("../data/image/logo-edited.png")}
+                className="h-12"
+              />
+            </Link>
+          </div>
+          <div className="mt-0">
+            <div 
+              // style={{
+              //   display: "flex",
+              //   flexDirection: "column",
+              //   alignItems: "center",
+              //   justifyContent: "center",
+              // }}  
+              className="grid justify-center"
+            >
+              {routes.map((route, index) => {
+                if (route.subRoutes) {
+                  return <SidebarMenu route={route} />;
+                }
+
+                return (
+                  <>
+                    <NavLink
+                      to={route.path}
+                      key={index}
+                      className={({ isActive }) =>
+                        isActive ? activeLink : normalLink
+                      }
+                    >
+                      <div
+                        style={{
+                          fontSize: "1.5rem",
+                          display: "flex",
+                          justifyContent: "center",
+                        }}
+                      >
+                        {route.icon}
+                      </div>
+                      <div 
+                        // style={{
+                        //   textTransform: "capitalize",
+                        //   display: "flex",
+                        //   justifyContent: "center",
+                        //   fontSize: "0.75rem",
+                        //   "@media only screen and (min-width:360px) and (max-width:500px)": {
+                        //     display: "none",
+                        //   }
+                        // }}
+                      >
+                        {route.name}
+                      </div>
+                    </NavLink>
+                  </>
+                );
+                // if (route.subRoutes) {
+                //   return <SidebarMenu route={route} />;
+                // }
+              })}
+            </div>
+          </div>
+        </div>
+      {/* )} */}
+      {/* {isSmallScreen && (
+        <div className="fixed bottom-0 left-0 right-0 z-50 bg-white pt-2">
+          <div className="flex justify-center items-center py-2">
+            <Link>
+              <img
+                src={require("../data/image/logo-edited.png")}
+                className="h-8"
+                onClick={openModal}
+              />
+            </Link>
+          </div>
+          <div className="flex justify-center ">
+            {isModal &&(
+              <div className="flex flex-row">
+                  {routes.map((route, index) => {
+                  if (route.subRoutes) {
+                    return <SidebarMenu 
+                    route={route} 
+                    // onDropDown={isModal}
+                    />;
+                  }
+                  return (
+                    <>
+                      <NavLink
+                        to={route.path}
+                        key={index}
+                        style={{
+                          fontSize: "2.5rem",
+                        }}
+                      >
+                          {route.icon}
+                      </NavLink>
+                    </>
+                  );
+                })}
+              </div>
+            )}
+          </div> 
+        </div>
+        
+        
+      )} */}
+      
+    </>
+    // <>
+    //     <div className="h-screen pb-10 pt-2 z-50">
+    //       <div className="flex justify-center items-center">
+    //          <Link>
+    //            <img
+    //              src={require("../data/image/logo-edited.png")}
+    //              className="h-12"
+    //            />
+    //          </Link>
+    //       </div>
+    //       <div className="mt-0">
+    //         <div className="grid justify-center">
+    //           {routes.map((route) => {
+    //             return <SidebarMenu route={route}/>;               
+    //           })}
+    //         </div>
+    //       </div>
+    //     </div>
+    // </>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/SidebarMenu.jsx
@@ -0,0 +1,187 @@
+import { react, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import { useStateContext } from "../contexts/ContextProvider";
+
+// const menuAnimation = {
+//   hidden: {
+//     opacity: 0,
+//     height: 0,
+//     padding: 0,
+//     transition: { duration: 0.3, when: "afterChildren" },
+//   },
+//   show: {
+//     opacity: 1,
+//     height: "auto",
+//     transition: {
+//       duration: 0.3,
+//       when: "beforeChildren",
+//     },
+//   },
+// };
+// const menuItemAnimation = {
+//   hidden: (i) => ({
+//     padding: 0,
+//     x: "-100%",
+//     transition: {
+//       duration: (i + 1) * 0.1,
+//     },
+//   }),
+//   show: (i) => ({
+//     x: 0,
+//     transition: {
+//       duration: (i + 1) * 0.1,
+//     },
+//   }),
+// };
+
+const SidebarMenu = ({ route }) => {
+  const activeLink =
+    "flex  flex-col items-center gap-1 p-2 rounded-lg text-black font-semibold text-md m-2 bg-active-bg";
+  const normalLink =
+    "flex flex-col justify-center items-center gap-1 p-2 rounded-lg text-md font-semibold text-white dark:text-gray-200 m-2";
+
+  // const dropDown= props.onDropDown;
+
+  // const [isHovering,setHovering] = useState(false);
+  // const [isDrop,setIsDrop]=useState(dropDown);
+
+  // const toogle = () =>{
+  //   setHovering(!isHovering);
+  // };
+
+  const [subnav, setSubnav] = useState(false);
+
+  const showSubnav = () => setSubnav(!subnav);
+
+  return (
+    // <>
+      // <div
+      //   className={normalLink}
+      //   // onMouseOver={handleMouseEnter}
+      //   // onMouseOut={handleMouseLeave}
+      //   // onMouseEnter={handleMouseEnter}
+      //   // onMouseLeave={handleMouseLeave}
+      // >
+      //   <div
+      //     className={({ isActive }) => (isActive ? activeLink : normalLink)}
+      //     onClick={toogle}
+      //   >
+      //     <div
+      //       style={{
+      //         fontSize: "1.5rem",
+      //         display: "flex",
+      //         justifyContent: "center",
+      //         cursor: "pointer",
+      //       }}
+      //     >
+      //       {route.icon}
+      //     </div>
+      //     <div className="capitalize flex justify-center text-xs">
+      //       {route.name}
+      //     </div>
+      //   </div>
+      // </div>{" "}
+      // {isHovering && (
+      //   <div
+      //     //variants={menuAnimation}
+      //     // initial="hidden"
+      //     // animate="show"
+      //     // exit="hidden"
+      //     style={{
+      //       //borderLeft: "0.2px solid black",
+      //       background: "#7b8cb8",
+      //       // height: "60px", 
+      //       // display: "flex",
+      //       // alignItems: "center",
+      //       // textDecoration: "none",
+      //       // color: "#f5f5f5",
+      //       //</>font-size= 18px;
+
+           
+      //     }}
+      //     // onMouseEnter={handleMouseEnter}
+      //     // onMouseLeave={handleMouseLeave}
+      //   >
+      //     {route.subRoutes.map((subRoute, i) => (
+      //       //<div variants={menuItemAnimation} key={i} custom={i}>
+      //         <NavLink
+      //           to={subRoute.path}
+      //           className={({ isActive }) =>
+      //             isActive ? activeLink : normalLink
+      //           }
+      //         >
+      //           <div
+      //             // style={{
+      //             //   fontSize: "1rem",
+      //             //   display: "flex",
+      //             //   justifyContent: "center",
+      //             // }}
+      //             className="text-base display-flex justify-center hover:text-[#1b67cc]"
+      //           >
+      //             {subRoute.icon}
+      //           </div>
+      //           <div className="capitalize flex justify-center text-xs ml-1 hover:text-[#1b67cc]">
+      //             {subRoute.name}
+      //           </div>
+      //         </NavLink>
+      //       //</div>
+      //     ))}
+      //   </div>
+      // )}{" "}
+    // </>
+
+  <>
+    {/* <div className={normalLink}> */}
+      {/* <div className={({ isActive }) => (isActive ? activeLink : normalLink)}>  */}
+          <NavLink to={route.path}  className={({ isActive }) => (isActive ? activeLink : normalLink)} onClick={route.subRoutes && showSubnav}>
+            <div
+              style={{
+                fontSize: "1.5rem",
+                display: "flex",
+                justifyContent: "center",
+                cursor: "pointer",
+              }}
+            >
+              {route.icon}
+            </div>
+            <div className="capitalize flex justify-center text-xs">
+              {route.name}
+            </div>
+            {/* <div>
+              {item.subNav && subnav
+                ? item.iconOpened
+                : item.subNav
+                ? item.iconClosed
+                : null}
+            </div> */}
+          </NavLink>
+          {subnav &&
+              <div style={{background: "#7b8cb8"}}>
+                {route.subRoutes.map((subRoute) => (
+                  <NavLink
+                    to={subRoute.path}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    <div className="text-base display-flex justify-center hover:text-[#1b67cc]">
+                      {subRoute.icon}
+                    </div>
+                    <div className="capitalize flex justify-center text-xs ml-1 hover:text-[#1b67cc]">
+                      {subRoute.name}
+                    </div>
+                  </NavLink>
+                 ))}
+              </div>
+          }
+        {/* </div> */}
+      {/* </div> */}
+    </>
+
+
+
+  );
+};
+
+export default SidebarMenu;
